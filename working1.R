923c0e7de3f00e9c4b5b9226447994899cd1f8e0
La-Roux-85 <lmyeitz1128@gmail.com>
2025-01-10 16:23:23 -0500

    adding more test files

---
+++ working1.R
@@ -0,0 +1,15 @@
+# load libraries
+library(usethis)
+library(gitcreds)
+library(credentials)
+library(gert)
+library(gh)
+
+# add credentials
+# use_git_config(user.name = "Jane Dow", user.email = "YourEmail@xxx.xxx")
+
+# Set - up personal access token (run line below if you need to get a token)
+# create_github_token()
+
+# # Run this to set token
+# gitcreds_set()
